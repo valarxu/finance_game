29abfd68f09e83ac4525ec6e99ee924bd8a36467
valarxu <7781862@qq.com>
2026-02-01 21:20:06 +0800

    chore: 完善图标

--- web/src/app/manifest.ts
+++ web/src/app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#ffffff',
     icons: [
       {
-        src: '/icon',
+        src: '/icon.png',
         sizes: 'any',
         type: 'image/png',
       },
